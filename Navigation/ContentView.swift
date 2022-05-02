d5a7b1e2706600eb05cb565faf02afbe24516d94
Erick Bazán <erick.bazan@hey.com>
2022-05-02 00:06:03 -0500

    Initial Commit

---
+++ Navigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Erick Bazán on 02/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Erick Bazán on 02/05/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
