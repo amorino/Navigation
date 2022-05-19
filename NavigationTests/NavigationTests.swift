580c1662770a5e6c15d8d3ab96d8a7caf1254474
Erick Bazán <erick.bazan@hey.com>
2022-05-19 13:17:44 -0500

    🪴 Navigation links

--- NavigationTests/NavigationTests.swift
+++ NavigationTests/NavigationTests.swift
@@ -1,36 +1,94 @@
-//
-//  NavigationTests.swift
-//  NavigationTests
-//
-//  Created by Erick Bazán on 02/05/22.
-//
-
+import CasePaths
 import XCTest
 @testable import Navigation
 
-class NavigationTests: XCTestCase {
+class SwiftUINavigationTests: XCTestCase {
+  func testAddItem() throws {
+    let viewModel = InventoryViewModel()
+    viewModel.addButtonTapped()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    let itemToAdd = try XCTUnwrap((/InventoryViewModel.Route.add).extract(from: XCTUnwrap(viewModel.route)))
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    viewModel.add(item: itemToAdd)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    XCTAssertNil(viewModel.route)
+    XCTAssertEqual(viewModel.inventory.count, 1)
+    XCTAssertEqual(viewModel.inventory[0].item, itemToAdd)
+  }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  func testDeleteItem() {
+    let viewModel = InventoryViewModel(
+      inventory: [
+        .init(item: .init(name: "Keyboard", color: .red, status: .inStock(quantity: 1)))
+      ]
+    )
 
+    viewModel.inventory[0].deleteButtonTapped()
+
+    XCTAssertEqual(viewModel.inventory[0].route, .deleteAlert)
+    XCTAssertEqual(viewModel.route, .row(id: viewModel.inventory[0].item.id, route: .deleteAlert))
+
+    viewModel.inventory[0].deleteConfirmationButtonTapped()
+
+    XCTAssertEqual(viewModel.inventory.count, 0)
+    XCTAssertEqual(viewModel.route, nil)
+  }
+
+  func testDuplicateItem() throws {
+    let item = Item(name: "Keyboard", color: .red, status: .inStock(quantity: 1))
+    let viewModel = InventoryViewModel(
+      inventory: [
+        .init(item: item)
+      ]
+    )
+
+    viewModel.inventory[0].duplicateButtonTapped()
+
+    //    XCTAssertEqual(viewModel.inventory[0].route, .duplicate(item))
+    XCTAssertNotNil(
+      (/ItemRowViewModel.Route.duplicate)
+        .extract(from: try XCTUnwrap(viewModel.inventory[0].route))
+    )
+
+    let dupe = item.duplicate()
+    viewModel.inventory[0].duplicate(item: dupe)
+
+    XCTAssertEqual(viewModel.inventory.count, 2)
+    XCTAssertEqual(viewModel.inventory[0].item, item)
+    XCTAssertEqual(viewModel.inventory[1].item, dupe)
+    XCTAssertNil(viewModel.inventory[0].route)
+  }
+
+  func testEdit() async throws {
+    let item = Item(name: "Keyboard", color: .red, status: .inStock(quantity: 1))
+    let viewModel = InventoryViewModel(
+
+      inventory: [
+        .init(item: item)
+      ]
+    )
+
+    viewModel.inventory[0].setEditNavigation(isActive: true)
+
+    XCTAssertNotNil(
+      (/ItemRowViewModel.Route.edit)
+        .extract(from: try XCTUnwrap(viewModel.inventory[0].route))
+    )
+
+    var editedItem = item
+    editedItem.color = .blue
+    viewModel.inventory[0].route = .edit(editedItem)
+
+    viewModel.inventory[0].edit(item: editedItem)
+
+    XCTAssertEqual(viewModel.inventory[0].isSaving, true)
+
+    try await Task.sleep(nanoseconds: NSEC_PER_SEC + 100 * NSEC_PER_MSEC)
+
+    XCTAssertNil(viewModel.inventory[0].route)
+    XCTAssertNil(viewModel.route)
+    XCTAssertEqual(viewModel.inventory[0].item, editedItem)
+
+    XCTAssertEqual(viewModel.inventory[0].isSaving, false)
+  }
 }
